0cd723ca992e8bcc8e56424ed8f07535409e58a2
WenJenChun <daydreamsss@hotmail.com>
2023-04-24 11:48:43 +0800

    commit

--- app/seleniumApp/tasks.py
+++ app/seleniumApp/tasks.py
@@ -6,7 +6,8 @@ import random
 import csv
 import os
 from django.db.models import Q
-from modelCore.models import User
+from modelCore.models import User, HouseCase
+
 
 import environ
 
@@ -16,15 +17,82 @@ ROOT_DIR = (
 env = environ.Env()
 env.read_env(str(ROOT_DIR.path(".env")))
 
-def crawl_manager_cases_by_requests():
-    print('crawl cases')
-    url = f'https://doc.twse.com.tw/server-java/t57sb01?step=1&colorchg=1&co_id={stock_code}&year={tw_year}&mtype=F&'
-    
+def crawl_manager_cases_by_requests(url):
+    # url = 'https://www.591.com.tw/broker24177-sale'
+    sale_url = f'{url}-sale'
+    print(sale_url)
 
-    print(url)
+    # from seleniumApp.tasks import *
+    # crawl_manager_cases_by_requests('https://www.591.com.tw/broker17800')
+
     # payload = {'api_key': env('scraper_key'), 'url': url}
     payload = {'api_key': 'f23a160f9e3ca319f97bd613b2fa047f', 'url': url}
     resp = requests.get('http://api.scraperapi.com', params=payload)
+
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    nav = soup.find('div',{'id':'head-nav'})
+    li = nav.find('li',{'data-stat':'shop-detail-sale'})
+    total_case_number= li.find('span',{'class':'ft_13'}).getText().replace('(', '').replace(')', '')
+    print('總案件數' + total_case_number)
+    total_page_number = int(total_case_number[:1]) + 1
+    print('總頁數' + str(total_page_number))
+    shop_id = url.replace('https://www.591.com.tw/broker','')
+    print('shop id' + shop_id)
+
+
+    i = 0
+    # i 小於頁數
+    while i < total_page_number:
+        print(f'==第 {i} 頁==')
+        cases_url = f'https://www.591.com.tw/index.php?firstRow={i}0&totalRows={total_case_number}&shop_id={shop_id}&type=2&m=&o=12&module=shop&action=house'
+        cases_resp = requests.get(cases_url)
+        cases_soup = BeautifulSoup(cases_resp.text, 'html.parser')
+        print(cases_soup)
+        lis = cases_soup.find(id='photolist').find(id='photolist').find_all('li')
+
+        for li in lis:
+            try:
+                imageLink = li.find("div", {"class": "photo"}).find('a').find('img')['src']
+                title = li.find("div", {"class": "details"}).find('h3').find('span').find('a').find('strong').getText()
+                county = li.find("div", {"class": "details"}).find('p').find_all('a')[0].getText()
+                city = li.find("div", {"class": "details"}).find('p').find_all('a')[1].getText()
+                type = li.find("div", {"class": "details"}).find('p',{"class":"l3"}).find('span',{"class":"kind"}).getText().replace('，', '')
+                units = li.find("div", {"class": "area"}).getText()
+                price = li.find("div", {"class": "prices"}).find("p", {"class": "price"}).getText()
+                linkPid = li['id'].replace('zid', '')
+                caseLink = f"https://sale.591.com.tw/home/house/detail/2/{linkPid}.html"
+                
+                print(imageLink)
+                print(title)
+                print(county, city)
+                print(type)
+                print(units)
+                print(price)
+                print(caseLink)
+
+                # server 有此筆資料, 則不動
+                # server 沒這個資料, 則增加
+                # !!! server 有這個資料, 但 list 沒這個資料, 則刪除~ => 還沒做
+
+                if HouseCase.objects.filter(case_link=caseLink).count() == 0:
+                    houseCase = HouseCase()
+                    houseCase.title = title
+                    houseCase.address = county + city
+                    houseCase.type = type
+                    houseCase.units = units
+                    houseCase.price = price
+                    houseCase.image = imageLink
+                    houseCase.case_link = caseLink
+                    houseCase.save()
+
+            except Exception as e:
+                print(e)
+
+        i = i + 1
+
+
+
+
 
 def assign_crawl_manager_tasks():
     users = User.objects.all()
